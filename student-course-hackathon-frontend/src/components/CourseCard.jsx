fcef6e0f581c12c2b8af912bada8f08a221a18d6
Maciej Parol <maciejparol333@gmail.com>
2022-11-21 15:42:57 -0500

    created chalkboard

--- student-course-hackathon-frontend/src/components/CourseCard.jsx
+++ student-course-hackathon-frontend/src/components/CourseCard.jsx
@@ -2,9 +2,16 @@ import { Link } from 'react-router-dom';
 
 const CourseCard = ({course}) => {
     return (
-        <Link to={`/students/${course.id}`}>
-            <h2>{course.name}</h2>
-            <p>{course.grade}</p>
+        <Link   to={`/students/${course.id}`}>
+           <div className= 'coursecard'>
+            <img
+            src= 'https://i.imgur.com/zETEB9bm.jpg'
+            className="chalkboard"
+            alt="">
+            </img>
+             <h2>{course.name}</h2> 
+             </div>
+            
         </Link>
     )
 }
